00e8e625c2bed9a33874358f3abfe955d53685c8
pratibhamaurya25 <pratibhamaurya25@navgurukul.org>
2026-07-24 00:01:28 +0530

    Fix addProduct export issue

--- src/pages/admin/AddProduct.jsx
+++ src/pages/admin/AddProduct.jsx
@@ -1,13 +1,13 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {useNavigate} from "react-router-dom";
 import ProductForm from "../../components/ProductForm";
-import { addProduct } from "../../services/api";
+import { createProduct } from "../../services/api";
 
 function AddProduct() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const mutation = useMutation({
-    mutationFn: addProduct,
+    mutationFn: createProduct,
     onSuccess: () => {
       alert("Product Added Successfully");
     
